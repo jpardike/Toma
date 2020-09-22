d828044648f86f17cc6c1498b8ce34226fba31dd
jpardike2 <jpardike@gmail.com>
2020-09-22 11:29:30 -0700

    Add: boilerplate to index.html, reset to styles.css, Tomagatchi class to app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+
+class Tomagatchi {
+    constructor(name) {
+        this.name = name;
+        this.age = 0;
+        this.hunger = 1;
+        this.sleepiness = 1;
+        this.boredom = 1;
+    }
+}
