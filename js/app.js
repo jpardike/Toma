572f111b01c5ecf75a86652fe47e8b9b751ead81
jpardike2 <jpardike@gmail.com>
2020-09-23 10:03:44 -0700

    Add: use jquery to display tomagatchi metrics on document

--- js/app.js
+++ js/app.js
@@ -5,24 +5,32 @@
 class Tomagatchi {
     constructor(name) {
         this.name = name;
-        this.age = 0;
+        this.age = 2;
         this.hunger = 1;
         this.sleepiness = 1;
         this.boredom = 1;
     }
 }
 
+// function to create tomagatchi and display metrics
 const createToma = function (name) {
     const toma = new Tomagatchi(name);
-    console.log(toma);
+    // console.log();
+    $('.age-text').text(`AGE: ${toma.age}`);
+    $('.hunger-text').text(`HUNGER: ${toma.hunger}`);
+    $('.sleepiness-text').text(`SLEEPY: ${toma.sleepiness}`);
+    $('.boredom-text').text(`BORED: ${toma.boredom}`);
+    return toma;
 }
 
+// function that generates an image that represents the Tomagatchi
 const generateToma = function () {
     const $toma = $('<img />');
     $toma.addClass('toma');
     $toma.attr('src', 'https://picsum.photos/id/1025/50');
     $('.gameplay-section').append($toma);
 }
+
 
 // button that starts game
 $('.play-button').on('click', function() {
