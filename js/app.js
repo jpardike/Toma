c6f6f47b1cd693d1bb7ccc64ea358551cf55ee03
jpardike2 <jpardike@gmail.com>
2020-09-23 14:20:06 -0700

    Add: button events that update the metrics for the tomagatchi

--- js/app.js
+++ js/app.js
@@ -1,26 +1,29 @@
 
-
+let toma = null;
 
 // Tomagatchi class
 class Tomagatchi {
     constructor(name) {
         this.name = name;
-        this.age = 2;
-        this.hunger = 1;
-        this.sleepiness = 1;
-        this.boredom = 1;
+        this.age = 0;
+        this.hunger = 10;
+        this.sleepiness = 10;
+        this.boredom = 10;
     }
-}
+};
 
 // function to create tomagatchi and display metrics
 const createToma = function (name) {
-    const toma = new Tomagatchi(name);
-    // console.log();
+    toma = new Tomagatchi(name);
+    console.log(toma);
+    updateMetrics();
+};
+
+const updateMetrics = function () {
     $('.age-text').text(`AGE: ${toma.age}`);
     $('.hunger-text').text(`HUNGER: ${toma.hunger}`);
     $('.sleepiness-text').text(`SLEEPY: ${toma.sleepiness}`);
     $('.boredom-text').text(`BORED: ${toma.boredom}`);
-    return toma;
 }
 
 // function that generates an image that represents the Tomagatchi
@@ -29,26 +32,55 @@ const generateToma = function () {
     $toma.addClass('toma');
     $toma.attr('src', 'https://picsum.photos/id/1025/50');
     $('.gameplay-section').append($toma);
-}
+};
 
 // create right buttons function
-const rightButtonsGenerator = function () {
+const rightButtonsController = function () {
     const $newDiv = $('<div />');
     $newDiv.addClass('right-buttons');
     $('.gameplay-section').append($newDiv);
+
     const $feedButton = $('<button>FEED</button>');
+    $feedButton.addClass('button-feed');
+
     const $sleepButton = $('<button>SLEEP</button>');
+    $sleepButton.addClass('button-sleep');
+
     const $exerciseButton = $('<button>EXERCISE</button>');
+    $exerciseButton.addClass('button-exercise');
+
     $newDiv.append($feedButton);
     $newDiv.append($sleepButton);
     $newDiv.append($exerciseButton);
-}
+
+    // button events to interact with tomagatchi
+    $feedButton.on('click', function () {
+        console.log('feed button clicked');
+        toma.hunger = 1;
+        updateMetrics();
+    });
+
+    $sleepButton.on('click', function () {
+        console.log('sleep button clicked');
+        toma.sleepiness = 1;
+        updateMetrics();
+    });
+
+    $exerciseButton.on('click', function () {
+        console.log('exercise button clicked');
+        toma.boredom = 1;
+        updateMetrics();
+    })
+};
+
+
+
 
 // button that starts game
 $('.play-button').on('click', function() {
     // console.log('button clicked');
     createToma('Toma');
     generateToma();
-    rightButtonsGenerator();
+    rightButtonsController();
 });
 
